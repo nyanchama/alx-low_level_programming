ba6157cd41599b551e5782a750f6d5feff4f5689
nyanchama <obaranyanchama1@gmail.com>
2022-03-23 01:31:58 +0000

    tasks

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,18 +1,1 @@
-#include "main.h"
-/**
- * _puts - function that prints a string
- *
- * @str: A pointer to an int that will be changed
- *
- */
 
-void _puts(char *str)
-{
-	int i;
-
-	for(i = 0; str[i] != '\0'; i++)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n')
-}
