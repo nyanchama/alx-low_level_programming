aed81698052fe21231778cb8a1b4451381c2b0c8
nyanchama <obaranyanchama1@gmail.com>
2022-03-23 01:16:42 +0000

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,18 @@
 #include "main.h"
 /**
- * _puts -  function that prints a string
+ * _puts - function that prints a string
  *
  * @str: A pointer to an int that will be changed
  *
- * Return: void which means our answer is correct
  */
 
 void _puts(char *str)
 {
-	char *c;
-	int m;
+	int i;
 
-	c = str;
-
-	for (m = 0; c[m]; m++)
-{
-_putchar(c[m]);
-}
-_putchar('\n');
+	for(i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n')
 }
