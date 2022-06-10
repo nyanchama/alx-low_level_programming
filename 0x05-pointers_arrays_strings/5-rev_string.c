389023747f75f29056a6d67d02992b8c0d7a21a3
Nyanchama Obara <92072653+nyanchama@users.noreply.github.com>
2022-06-11 00:41:41 +0300

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,9 @@
-#includde "main.h"
+#include "main.h"
 
 /**
- * rev_string - function to reverse a string
+ * rev_string - reverse a string.
  *
- * @s: the string to be reversed
+ *@s: the string to be reversed.
  */
 
 void rev_string(char *s)
